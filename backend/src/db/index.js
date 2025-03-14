ef207a6e0955ecf4d2aac78f4f33ed2bd89be4b4
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-14 11:53:07 +0530

    setup env and start express server

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,6 +3,10 @@
 import { connect } from "mongoose";
 
 const connectDB = async () => {
+    if (!process.env.MONGO_URL) {
+        console.error("🚨 ERROR: MONGO_URL environment variable is not defined!");
+        process.exit(1); // Exit the process if MONGO_URL is missing
+    }
     try {
         const connectionInstance = await connect(process.env.MONGO_URL);
         console.log(`🛠️  MONGODB Connected! DB HOST: ${connectionInstance.connection.host} DB NAME: ${connectionInstance.connection.db.databaseName}`);
