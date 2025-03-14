b38926d997f55c66dd453dfdea04e09ab94ea69e
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-15 00:25:01 +0530

    refactor: replace body-parser with express built-in middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,12 @@
 import express from "express";
 import { adminRoutes } from "./routes/index.routes.js";
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
 // Middleware
-app.use(bodyParser.json({ limit: "16kb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
 
 // Routes
