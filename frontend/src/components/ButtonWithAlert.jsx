1908320d51724bc668eef91d1e227f240ebb8719
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-21 23:49:55 +0530

    feat: add button with alert component for confirmation of action

---
+++ frontend/src/components/ButtonWithAlert.jsx
@@ -0,0 +1,49 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+const ButtonWithAlert = ({
+    buttonTitle,
+    buttonColor = "Danger",
+    dialogTitle,
+    dialogDesc,
+    dialogCancelTitle = "Cancel",
+    dialogActionTitle = "Continue",
+    dialogActionBtnColor = "Primary",
+    dialogActionfunc,
+    className,
+    ...props
+}) => {
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button className={`${buttonColor} ${className}`} {...props}>
+                    {buttonTitle}
+                </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent className="bg-white dark:bg-slate-950">
+                <AlertDialogHeader>
+                    <AlertDialogTitle>{dialogTitle}</AlertDialogTitle>
+                    {dialogDesc && <AlertDialogDescription>{dialogDesc}</AlertDialogDescription>}
+                </AlertDialogHeader>
+                <AlertDialogFooter className="mt-2">
+                    <AlertDialogCancel>{dialogCancelTitle}</AlertDialogCancel>
+                    <AlertDialogAction onClick={dialogActionfunc} className={dialogActionBtnColor}>
+                        {dialogActionTitle}
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+};
+
+export default ButtonWithAlert;
