5e52b6c6e7510ed2ccc4a52c67d7b4cdc72cbfa7
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-21 10:47:08 +0530

    feat: add logout functionality

---
+++ frontend/src/admin/component/AdminLogoutBtn.jsx
@@ -0,0 +1,64 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "@/components/ui/button";
+import toastService from "@/services/toastService";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useMutation } from "@tanstack/react-query";
+import crudService from "@/services/crudService";
+import { logout } from "@/store/features/admin/admin/adminAuthSlice";
+import { storePersistor } from "@/store/store";
+
+const LogoutBtn = () => {
+    const dispatch = useDispatch();
+    const { admin } = useSelector(state => state.adminAuth);
+
+    const { mutate } = useMutation({
+        mutationFn: () => crudService.post("auth/logout", true),
+        onSuccess: data => {
+            dispatch(logout());
+            toastService.info(data?.message);
+            storePersistor.purge();
+        },
+        onError: error => {
+            toastService.error(error.message || "Something Went Wrong While Log-Out");
+        },
+    });
+
+    const logoutHandler = () => {
+        if (admin?._id) {
+            mutate(admin?._id);
+        }
+    };
+    return (
+        <>
+            <AlertDialog>
+                <AlertDialogTrigger asChild>
+                    <Button className="Danger w-full">Log Out</Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent className="bg-white dark:bg-slate-950">
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Confirm Logout</AlertDialogTitle>
+                        <AlertDialogDescription>Are you sure you want to log out? This will end your current session.</AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction className="Danger" onClick={logoutHandler}>
+                            Continue
+                        </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+        </>
+    );
+};
+
+export default LogoutBtn;
--- frontend/src/components/SideBar/AdminSideBar.jsx
+++ frontend/src/components/SideBar/AdminSideBar.jsx
@@ -5,9 +5,10 @@ import { AiFillDashboard } from "react-icons/ai";
 import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, useSidebar } from "@/components/ui/sidebar";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
 import { Link, useLocation } from "react-router-dom";
-import { BadgeCheck, ChevronRight, ChevronsUpDown, LogOut } from "lucide-react";
+import { BadgeCheck, ChevronRight, ChevronsUpDown } from "lucide-react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import LogoutBtn from "@/admin/component/AdminLogoutBtn";
 
 const navBar = [
     { name: "Main Site", Icon: FaHome, urlLink: "/" },
@@ -160,12 +161,12 @@ const AdminSideBar = ({ user, ...props }) => {
                                 <DropdownMenuLabel className="p-0 font-normal">
                                     <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                                         <Avatar className="h-8 w-8 rounded-lg">
-                                            <AvatarImage src="https://ui.shadcn.com/avatars/shadcn.jpg" alt={user.username} />
+                                            <AvatarImage src="https://ui.shadcn.com/avatars/shadcn.jpg" alt={user?.username} />
                                             <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                                         </Avatar>
                                         <div className="grid flex-1 text-left text-sm leading-tight">
-                                            <span className="truncate font-semibold">{user.username}</span>
-                                            <span className="truncate text-xs">{user.email}</span>
+                                            <span className="truncate font-semibold">{user?.username}</span>
+                                            <span className="truncate text-xs">{user?.email}</span>
                                         </div>
                                     </div>
                                 </DropdownMenuLabel>
@@ -175,9 +176,8 @@ const AdminSideBar = ({ user, ...props }) => {
                                         <BadgeCheck />
                                         Account
                                     </DropdownMenuItem>
-                                    <DropdownMenuItem>
-                                        <LogOut />
-                                        Log out
+                                    <DropdownMenuItem onSelect={e => e.preventDefault()}>
+                                        <LogoutBtn />
                                     </DropdownMenuItem>
                                 </DropdownMenuGroup>
                             </DropdownMenuContent>
