41992712a3633ac5da3e90e5739cf79a18a5c39b
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-21 08:48:02 +0530

    fix: resolve the cors issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { adminRoutes } from "./routes/index.routes.js";
 import cookieParser from "cookie-parser";
 const app = express();
 
 // Middleware
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
