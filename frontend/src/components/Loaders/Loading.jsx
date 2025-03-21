1144d5ccfd87cf4e9e3d80ca2455752f0376b312
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-21 09:03:02 +0530

    styles: add backdrop

--- frontend/src/components/Loaders/Loading.jsx
+++ frontend/src/components/Loaders/Loading.jsx
@@ -1,7 +1,7 @@
-const Loading = ({ width = 14, height = 14 }) => {
+const Loading = ({ width = 50, height = 50 }) => {
     return (
-        <div className="w-screen h-screen bg-black/50 !overflow-hidden pointer-events-none z-50 transition-all duration-100 ease-in-out flex items-center justify-center">
-            <div className={`w-${width} h-${height} border-6 border-solid border-blue-600 rounded-full border-t-transparent animate-spin`}></div>
+        <div className="w-screen h-screen fixed top-0 left-0 bottom-0 pointer-events-none z-50 flex items-center justify-center bg-black/70  backdrop-blur-md transition-all duration-100 ease-in-out ">
+            <div style={{ width: `${width}px`, height: `${height}px` }} className="border-6 border-solid border-blue-600 rounded-full border-t-transparent animate-spin"></div>
         </div>
     );
 };
