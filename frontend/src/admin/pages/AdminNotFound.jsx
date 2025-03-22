4db058971cb310b59724ae7b041f67f1c98d43bf
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-22 16:20:52 +0530

    style: add cursor-point

--- frontend/src/admin/pages/AdminNotFound.jsx
+++ frontend/src/admin/pages/AdminNotFound.jsx
@@ -20,7 +20,7 @@ const AdminNotFound = () => {
                 <h2 className="font-bold text-xl mb-3 lg:text-4xl xl:text-4xl 2xl:text-4xl">The Page You&apos;re Looking For Can&apos;t Found!</h2>
                 <p className="leading-7 text-base font-medium mb-4">You didn&apos;t break the internet, but we can&apos;t find what you are looking for.</p>
                 <Link to={"/admin/dashboard"}>
-                    <Button className="Primary btn2xl">Back To Dashboard</Button>
+                    <Button className="Primary btn2xl cursor-pointer">Back To Dashboard</Button>
                 </Link>
             </section>
         </>
