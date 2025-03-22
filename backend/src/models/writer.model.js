c26d619387f20569da633c0f02c440217b995415
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-22 22:23:21 +0530

    fix: arrow function issue is fix

--- backend/src/models/writer.model.js
+++ backend/src/models/writer.model.js
@@ -67,11 +67,11 @@ writerScheme.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
 
-writerScheme.methods.generateAccessToken = () => {
+writerScheme.methods.generateAccessToken = function () {
     return jwt.sign({ _id: this._id, email: this.email, fullName: `${this.firstName} ${this.lastName}` }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY });
 };
 
-writerScheme.methods.generateRefreshToken = () => {
+writerScheme.methods.generateRefreshToken = function () {
     return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY });
 };
 
