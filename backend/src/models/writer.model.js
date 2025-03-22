2dc606679c450816d3ab1fb12ce2b2e047becd92
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-22 16:57:50 +0530

    feat: add new secure feature
    - add password hashing with bcrypt
    - add function for checking the password is correct or not
    - add methods for generating the access token and refresh token

--- backend/src/models/writer.model.js
+++ backend/src/models/writer.model.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const writerScheme = new Schema(
     {
         firstName: {
@@ -46,5 +49,30 @@ const writerScheme = new Schema(
     },
     { timestamps: true }
 );
+
+writerScheme.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    try {
+        this.password = await bcrypt.hash(this.password, 10);
+    } catch (error) {
+        next(error);
+    }
+
+    return next();
+});
+
+writerScheme.methods.isCorrectPassword = async (password) => {
+    return await bcrypt.compare(password, this.password);
+};
+
+writerScheme.methods.generateAccessToken = () => {
+    return jwt.sign({ _id: this._id, email: this.email, fullName: `${this.firstName} ${this.lastName}` }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY });
+};
+
+writerScheme.methods.generateRefreshToken = () => {
+    return jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY });
+};
 
 export const Writer = mongoose.model("Writer", writerScheme);
