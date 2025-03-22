054602f293ca30974815608c5f8914efe9a84123
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-22 11:50:09 +0530

    chore: add router to use writer routes

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,6 +3,7 @@ import authRouter from "./admin/auth.routes.js";
 import categoryRouter from "./admin/category.routes.js";
 import blogRouter from "./admin/blog.routes.js";
 import moderatorRouter from "./admin/moderator.routes.js";
+import writerRouter from "./admin/writeAuth.routes.js";
 
 // Admin Routes
 const adminRoutes = Router();
@@ -10,5 +11,6 @@ adminRoutes.use("/auth", authRouter);
 adminRoutes.use("/category", categoryRouter);
 adminRoutes.use("/blog", blogRouter);
 adminRoutes.use("/moderator", moderatorRouter);
+adminRoutes.use("/writer", writerRouter);
 
 export { adminRoutes };
