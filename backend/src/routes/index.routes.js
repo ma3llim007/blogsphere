a17971c253e6747958dbe4f5b826fc9ac818135f
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-24 23:15:28 +0530

    fix: resolve routes name conflict

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,31 +1,31 @@
 import { Router } from "express";
 // Admin Router
-import authRouter from "./admin/auth.routes.js";
-import categoryRouter from "./admin/category.routes.js";
-import blogRouter from "./admin/blog.routes.js";
-import moderatorRouter from "./admin/moderator.routes.js";
-import writerRouter from "./admin/writeAuth.routes.js";
+import admin_authRouter from "./admin/auth.routes.js";
+import admin_categoryRouter from "./admin/category.routes.js";
+import admin_blogRouter from "./admin/blog.routes.js";
+import admin_moderatorRouter from "./admin/moderator.routes.js";
+import admin_writerRouter from "./admin/writeAuth.routes.js";
 // Moderator Router
-import moderatorAuthRouter from "./moderator/auth.routes.js";
+import moderator_AuthRouter from "./moderator/auth.routes.js";
 // Writer Router
-import writerAuthRouter from "./writer/auth.routes.js";
-import writerBlogRouter from "./writer/blog.routes.js";
+import writer_AuthRouter from "./writer/auth.routes.js";
+import writer_BlogRouter from "./writer/blog.routes.js";
 
 // Admin Routes
 const adminRoutes = Router();
-adminRoutes.use("/auth", authRouter);
-adminRoutes.use("/category", categoryRouter);
-adminRoutes.use("/blog", blogRouter);
-adminRoutes.use("/moderator", moderatorRouter);
-adminRoutes.use("/writer", writerRouter);
+adminRoutes.use("/auth", admin_authRouter);
+adminRoutes.use("/category", admin_categoryRouter);
+adminRoutes.use("/blog", admin_blogRouter);
+adminRoutes.use("/moderator", admin_moderatorRouter);
+adminRoutes.use("/writer", admin_writerRouter);
 
 // Moderator Routes
 const moderatorRoutes = Router();
-moderatorRouter.route("/auth", moderatorAuthRouter);
+moderatorRoutes.use("/auth", moderator_AuthRouter);
 
 // Writer Routes
 const writerRoutes = Router();
-writerRoutes.use("/auth", writerAuthRouter);
-writerRoutes.use("/blog", writerBlogRouter);
+writerRoutes.use("/auth", writer_AuthRouter);
+writerRoutes.use("/blog", writer_BlogRouter);
 
 export { adminRoutes, writerRoutes, moderatorRoutes };
