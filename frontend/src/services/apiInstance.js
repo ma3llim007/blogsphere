aace9f2e7c75b9f2aacbfbe2ea174a3b9f43060c
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-02 18:13:43 +0530

    refactor: remove global Error Handle

--- frontend/src/services/apiInstance.js
+++ frontend/src/services/apiInstance.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import toastService from "./toastService";
 
 // Public Axios instance
 const axiosInstance = axios.create({
@@ -7,15 +6,5 @@ const axiosInstance = axios.create({
     timeout: 15000,
     withCredentials: true,
 });
-
-// Global response interceptor (optional)
-axiosInstance.interceptors.response.use(
-    response => response,
-    error => {
-        const errorMessage = error.response?.data?.message || "Something Went Wrong!";
-        toastService.error(`API ERROR: ${errorMessage}`);
-        return Promise.reject(errorMessage);
-    }
-);
 
 export default axiosInstance;
