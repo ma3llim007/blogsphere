8f0ef3a246aea3b277acf4fc11ecf121a5775f1e
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-04 11:52:19 +0530

    style: improve the title

--- frontend/src/components/client/PageBanner.jsx
+++ frontend/src/components/client/PageBanner.jsx
@@ -6,7 +6,9 @@ const PageBanner = ({ title, children }) => {
         <div className="bg-light py-16">
             <Container>
                 <div className="flex justify-between items-center">
-                    <h2 className="text-center border-l-4 sm:text-start pl-4 border-blue-600 rounded-l-lg text-3xl font-extrabold py-1">{capitalizeWords(title)}</h2>
+                    <h2 className="line-clamp-3 break-words text-2xl sm:text-3xl md:text-4xl font-extrabold leading-snug border-l-4 sm:text-start pl-4 border-blue-600 rounded-l-lg py-1 max-w-full md:max-w-md lg:max-w-2xl xl:max-w-4xl 2xl:max-w-6xl">
+                        {capitalizeWords(title)}
+                    </h2>
                     <div className="justify-center items-center hidden md:flex my-2 text-lg">{children}</div>
                 </div>
             </Container>
