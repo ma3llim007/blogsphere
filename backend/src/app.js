df852a56bda61e3595100c7ad3e3f6b3636db5bc
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-07 21:36:56 +0530

    chore: remove the morgan

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,4 @@
 import express from "express";
-import morgan from "morgan";
 import cors from "cors";
 import { adminRoutes, clientRoutes, moderatorRoutes, writerRoutes } from "./routes/index.routes.js";
 import cookieParser from "cookie-parser";
@@ -12,7 +11,6 @@ app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
-app.use(morgan("combined"));
 app.use(
     helmet({
         contentSecurityPolicy: false,
