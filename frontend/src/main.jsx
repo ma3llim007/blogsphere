39734c1754be42c384bb6c4d4f348aa5efc58372
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-10 19:25:09 +0530

    chore: remove the responseViewer and react-query-devtools

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,8 @@ import { store, storePersistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./services/queryClientConfig";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ToastContainer } from "react-toastify";
 import { HelmetProvider } from "react-helmet-async";
-import ResponsiveViewer from "./components/ResponsiveViewer";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
@@ -19,9 +17,7 @@ createRoot(document.getElementById("root")).render(
                 <QueryClientProvider client={queryClient}>
                     <HelmetProvider>
                         <App />
-                        <ResponsiveViewer />
                     </HelmetProvider>
-                    <ReactQueryDevtools initialIsOpen={false} />
                     <ToastContainer limit={4} />
                 </QueryClientProvider>
             </PersistGate>
